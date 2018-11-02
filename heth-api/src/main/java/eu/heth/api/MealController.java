89b5049f5e29faa9c77afee2715b442f81f7652b
anaspleen <thomas.caiati@gmail.com>
2018-11-02 10:45:05 +0100

    Add API in progress with Spring Boot

---
+++ heth-api/src/main/java/eu/heth/api/MealController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package eu.heth.api;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import eu.heth.bean.Meal;
+import eu.heth.bean.MealBean;
+
+/**
+ * @author tomey
+ *
+ */
+@RestController
+public class MealController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/meal")
+	public Meal meal(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new MealBean(counter.incrementAndGet() + "", String.format(template, name));
+	}
+}
---
+++ heth-api/src/main/java/eu/heth/api/main/Application.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package eu.heth.api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tomey
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
